ea8363cc6abff3cb266026d69f55ba80af03f808
RenatoCP <renato_pinha@icloud.com>
2021-11-23 14:48:38 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const writeCards = (msgName, msgEvent) => {
+    let message = []
+
+    for(let i = 0; i < msgName.length; i++) {
+    console.log(msgName[i]);
+    message.push(`Thank you, ${msgName[i]}, for the wonderful ${msgEvent} gift!`);
+    }
+
+    return message;
+}
+
+const countDown = () => {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+    return countDown;
+}
